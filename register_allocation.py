1811537cc9c0d496ec0ffb5e669853f74e0898e4
Pleket <50240262+Pleket@users.noreply.github.com>
2023-10-18 16:11:47 +0200

    Testing with optimalisation

--- register_allocation.py
+++ register_allocation.py
@@ -6,10 +6,10 @@ file = open("./big-dimacs/fpsol2.i.1.col", "r")
 coloring = Function('coloring', IntSort(), IntSort())
 k = Int('k')
 
-o = Optimize()
+o = Goal()
 
-i = 1
-limit = 1000
+# i = 1
+# limit = 5000
 for row in file:
     match row[0]:
         case 'c':
@@ -17,14 +17,26 @@ for row in file:
         case 'p':
             rows = row.split(' ')
             for i in range(1, int(rows[2]) + 1):
-                o.add(And(coloring(i) >= 1, coloring(i) <= k))
+                o.add(And(coloring(i) > 0, coloring(i) < k + 1))
         case 'e':
             rows = row.split(' ')
-            o.add(coloring(int(rows[1])) != coloring(int(rows[2])))
-    i += 1
-    if i > limit:
-        break
+            o.add(Or(coloring(int(rows[1])) < coloring(int(rows[2])), coloring(int(rows[1])) > coloring(int(rows[2]))))
+    # i += 1
+    # if i > limit:
+    #     break
+o.add(k == 30)
 
-o.minimize(k)
-o.check()
-o.model()
+# o.minimize(k)
+# print(o.check())
+# print(o.model())
+
+t = Then('simplify', 'normalize-bounds', 'solve-eqs')
+r = t(o)
+print(r)
+print(r[0])
+
+s = Optimize()
+s.add(r[0])
+print(s.check())
+print(s.model())
+# print(r.convert_model(s.model()))
