79085f0de830fa24e3c08c55268a2ab872d01d77
Pleket <50240262+Pleket@users.noreply.github.com>
2023-10-18 15:08:39 +0200

    Data and new approach

--- register_allocation.py
+++ register_allocation.py
@@ -1,12 +1,12 @@
 from z3 import *
 import numpy as np
 
-file = open("example.col", "r")
+file = open("./big-dimacs/fpsol2.i.1.col", "r")
 
 n = 0
 m = 0
 edges = []
-n_nb = np.zeros(n)
+# n_nb = np.zeros(n)
 for row in file:
     match row[0]:
         case 'c':
@@ -19,36 +19,46 @@ for row in file:
             rows = row.split(' ')
             edges.append((int(rows[1]), int(rows[2])))
             edges.append((int(rows[2]), int(rows[1])))
-            n_nb[int(rows[1]) - 1] += 1
-            n_nb[int(rows[2]) - 1] += 1
+            # n_nb[int(rows[1]) - 1] += 1
+            # n_nb[int(rows[2]) - 1] += 1
 edges.sort()
 
-EdgesSort = Datatype('EdgesSort')
-EdgesSort.declare('EdgesSort', ArraySort(IntSort(), IntSort()))
+# EdgesSort = Datatype('EdgesSort')
+# EdgesSort.declare('EdgesSort', ArraySort(IntSort(), IntSort()))
 
 coloring = Function('coloring', IntSort(), IntSort())
-neighbors = Function('neighbors', IntSort(), EdgesSort())
+# neighbors = Function('neighbors', IntSort(), EdgesSort())
 # conflict = Function('conflict', EdgesSort(), BoolSort())
 k = Int('k')
 
 o = Optimize()
 
-j = 0
-old_node = 0
-for i in range(1, m + 1):
-    if edges[i][0] != old_node:
-        j = 0
-        old_node = edges[i][0]
-    o.add(neighbors(edges[i][0])[j] == edges[i][1])
-    j += 1
+# j = 0
+# old_node = 0
+# for i in range(1, m + 1):
+#     if edges[i][0] != old_node:
+#         j = 0
+#         old_node = edges[i][0]
+#     o.add(neighbors(edges[i][0])[j] == edges[i][1])
+#     j += 1
+
+# for i in range(1, n + 1):
+#     o.add(And(coloring(i) >= 1, coloring(i) <= k))
+
+#     o.add(neighbors(i).range() == n_nb[i - 1])
+
+# for i in range(1, n + 1):
+#     Int('j')
+#     o.add(ForAll(neighbors(i)[j], coloring(i) != coloring(neighbors(i)[j])))
+
+# minimize(o)
 
 for i in range(1, n + 1):
     o.add(And(coloring(i) >= 1, coloring(i) <= k))
 
-    o.add(neighbors(i).range() == n_nb[i - 1])
+for i in range(0, m):
+    o.add(coloring(edges[i][0]) != coloring(edges[i][1]))
 
-for i in range(1, n + 1):
-    Int('j')
-    o.add(ForAll(neighbors(i)[j], coloring(i) != coloring(neighbors(i)[j])))
-
-minimize(o)
+o.minimize(k)
+o.check()
+o.model()
