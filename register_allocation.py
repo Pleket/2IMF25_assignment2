1be5032989cb1a9c2fc842bdb2cda88c091e6d88
Pleket <50240262+Pleket@users.noreply.github.com>
2023-10-22 23:06:35 +0200

    Bugfixes

--- register_allocation.py
+++ register_allocation.py
@@ -44,6 +44,9 @@ o.add(k == 30)
 
 bdd = bdd.BDD()
 vert = 0
+first = True
+u = None
+file.seek(0)
 for row in file:
     match row[0]:
         case 'c':
@@ -57,18 +60,25 @@ for row in file:
             for j in range(1, 6):
                 bdd.add_var(f"k_{j}")
             # for i in range(1, vert + 1):
-            #     bdd.add_expr(f"(k_{5} | !x_{i}_{5}) & 
+            #     z = bdd.add_expr(f"(k_{5} | !x_{i}_{5}) & 
             #                     (!(!k_{5} & !k_{4}) | !x_{i}_{4}) &
             #                     (!(!k_{5} & !k_{4} & !k_{3}) | !x_{i}_{3}) &
             #                     (!(!k_{5} & !k_{4} & !k_{3} & !k_{2}) | !x_{i}_{2})")
+            # if first:
+            #     u = z
+            #     first = False
         case 'e':
             rows = row.split(' ')
             a = int(rows[1])
             b = int(rows[2])
-            bdd.add_expr(f"""!((b_{a}_{5} & b_{b}_{5}) | (!b_{a}_{5} & !b_{b}_{5}))) |
-                            !((b_{a}_{4} & b_{b}_{4}) | (!b_{a}_{4} & !b_{b}_{4}))) |
-                            !((b_{a}_{3} & b_{b}_{3}) | (!b_{a}_{3} & !b_{b}_{3}))) |
-                            !((b_{a}_{2} & b_{b}_{2}) | (!b_{a}_{2} & !b_{b}_{2}))) |
-                            !((b_{a}_{1} & b_{b}_{1}) | (!b_{a}_{1} & !b_{b}_{1}))) |""")
+            z = bdd.add_expr(f"""!((x_{a}_{5} & x_{b}_{5}) | (!x_{a}_{5} & !x_{b}_{5})) |
+                            !((x_{a}_{4} & x_{b}_{4}) | (!x_{a}_{4} & !x_{b}_{4})) |
+                            !((x_{a}_{3} & x_{b}_{3}) | (!x_{a}_{3} & !x_{b}_{3})) |
+                            !((x_{a}_{2} & x_{b}_{2}) | (!x_{a}_{2} & !x_{b}_{2})) |
+                            !((x_{a}_{1} & x_{b}_{1}) | (!x_{a}_{1} & !x_{b}_{1}))""")
+            if first:
+                u = z
+            # print(u)
+            # print(u.negated)
 
-bdd.count()
+print(bdd.count(u))
